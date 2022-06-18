77b0e8791ffc3a6aafeedb509bc1de1611134adc
Binij Shrestha <shresthabinij@gmail.com>
2022-06-18 11:33:10 +0545

    v0.0.4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="flex flex-col items-center justify-center w-full mx-auto gap-10">
-	<h1 class="text-4xl">Sample</h1>
+	<h1 class="text-4xl">Example</h1>
 	<NepaliDatePicker selectedDate="2079/02/30" />
 </div>
