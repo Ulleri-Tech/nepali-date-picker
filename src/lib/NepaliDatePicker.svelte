547a46c4e7c21dd0a4700363216c2f622949bb69
Binij Shrestha <shresthabinij@gmail.com>
2022-06-29 17:10:28 +0545

    Remove Unnecessary

--- src/lib/NepaliDatePicker.svelte
+++ src/lib/NepaliDatePicker.svelte
@@ -29,61 +29,61 @@
 		rows = slices.map((start) => daysArr.slice(start, start + 7));
 		rows = rows.filter((row: any) => row[0] > 0 || row[6] > 0);
 	}
+
+	const handleDropdownClick = () => {
+		isOpenCalendar = !isOpenCalendar; // togle state on click
+	};
+
+	const handleDropdownFocusLoss = ({ relatedTarget, currentTarget }) => {
+		if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return; // check if the new focus target doesn't present in the dropdown tree (exclude ul\li padding area because relatedTarget, in this case, will be null)
+		isOpenCalendar = false;
+	};
 </script>
 
-{#if isOpenCalendar}
-	<Calender
-		bind:selectedMonth
-		bind:selectedYear
-		bind:selectedDay
-		bind:selectedDate
-		bind:rows
-		{updateRows}
-	/>
-	<!-- <div class="overflow-auto fixed z-10 mt-16">
-		<div class="flex justify-center items-center py-3 px-1">
-			<div class="w-full max-w-sm shadow-lg">
-				<div class="p-2 bg-white rounded-t sm:p-4">
-					<div class="flex justify-between items-center px-2">
-						header
-					</div>
-					<div class="flex overflow-x-auto justify-between items-center pt-2">
-						
+<div class="flex justify-between items-center">
+	<div class="dropdown" on:focusout={handleDropdownFocusLoss}>
+		<button class="btn m-1" on:click={handleDropdownClick}>
+			<div class="flex flex-col gap-1 w-36">
+				<label for="date" class="mb-2 h-4 font-bold">Date</label>
+				<div
+					class="flex items-center ml-0.5 p-4 h-9 bg-white outline outline-[0.5px] solid outline-gray-500"
+				>
+					<input
+						placeholder="YYYY/MM/DD"
+						type="text"
+						bind:value={selectedDate}
+						disabled
+						class="w-24 bg-white"
+					/>
+					<div class="flex">
+						<svg
+							width="14"
+							height="20"
+							viewBox="0 0 40 44"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M36 4H34V0H30V4H10V0H6V4H4C1.8 4 0 5.8 0 8V40C0 42.2 1.8 44 4 44H36C38.2 44 40 42.2 40 40V8C40 5.8 38.2 4 36 4ZM36 40H4V14H36V40Z"
+								fill="black"
+								fill-opacity="0.56"
+							/>
+						</svg>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div> -->
-{/if}
-<div class="flex flex-col gap-1 w-36">
-	<label for="date" class="mb-2 h-4 font-bold">Date</label>
-	<div
-		on:click={() => {
-			isOpenCalendar = !isOpenCalendar;
-		}}
-		class="flex items-center ml-0.5 p-4 h-9 bg-white outline outline-[0.5px] solid outline-gray-500"
-	>
-		<input
-			placeholder="YYYY/MM/DD"
-			type="text"
-			bind:value={selectedDate}
-			disabled
-			class="w-24 bg-white"
-		/>
-		<div class="flex">
-			<svg
-				width="14"
-				height="20"
-				viewBox="0 0 40 44"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M36 4H34V0H30V4H10V0H6V4H4C1.8 4 0 5.8 0 8V40C0 42.2 1.8 44 4 44H36C38.2 44 40 42.2 40 40V8C40 5.8 38.2 4 36 4ZM36 40H4V14H36V40Z"
-					fill="black"
-					fill-opacity="0.56"
-				/>
-			</svg>
+		</button>
+
+		<div class="absolute" style:visibility={isOpenCalendar ? 'visible' : 'hidden'}>
+			<Calender
+				bind:open={isOpenCalendar}
+				bind:selectedMonth
+				bind:selectedYear
+				bind:selectedDay
+				bind:selectedDate
+				bind:rows
+				{updateRows}
+			/>
 		</div>
 	</div>
 </div>
--- src/lib/components/Calender.svelte
+++ src/lib/components/Calender.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
-	import { clickOutside } from '../utils';
-	import NepaliDate from 'nepali-date-converter';
-	import ShiftMonth from './ShiftMonth.svelte';
-	import CalenderTable from './CalenderTable.svelte';
-
 	export let selectedMonth: number;
 	export let selectedYear: number;
 	export let selectedDate: any;
@@ -11,13 +6,24 @@
 	export let rows: any;
 	export let updateRows: any;
 	export let open: boolean;
+	import NepaliDate from 'nepali-date-converter';
+	import ShiftMonth from './ShiftMonth.svelte';
+
+	const arrDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	const currentMonth = new NepaliDate().getBS().month + 1; // 1..12
+	const currentYear = new NepaliDate().getBS().year; // 2078...
+	const currentDay = new NepaliDate().getBS().date; // 1..31
+
+	function selectDate(y: any, m: any, d: any) {
+		selectedDay = d;
+		selectedDate = new NepaliDate(y, m - 1, d).format('YYYY/MM/DD');
+		open = false;
+	}
+
+	$: inCurrentMonth = currentMonth === selectedMonth && currentYear === selectedYear;
 </script>
 
-<div
-	class="main"
-	use:clickOutside={{ enabled: open, cb: () => (open = false) }}
-	on:click|preventDefault={() => (open = !open)}
->
+<div class="main">
 	<div class="header justify-between">
 		<span class="font-bold text-sm text-gray-800 sm:text-base focus:outline-none">
 			{new NepaliDate(selectedYear, selectedMonth - 1, 1).format('MMMM YYYY')}
@@ -25,14 +31,57 @@
 		<ShiftMonth bind:selectedMonth bind:selectedYear {updateRows} />
 	</div>
 
-	<CalenderTable
-		bind:selectedMonth
-		bind:selectedYear
-		bind:selectedDay
-		bind:selectedDate
-		bind:rows
-		bind:open
-	/>
+	<table class="w-full">
+		<thead>
+			<tr>
+				{#each arrDays as day}
+					<th>
+						<div class="flex justify-center w-full">
+							<p class="text-sm font-medium text-center text-gray-800 sm:text-base">
+								{day}
+							</p>
+						</div>
+					</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each rows as col}
+				<tr>
+					{#each col as i}
+						<td>
+							<div class="flex justify-center p-2 w-full cursor-pointer">
+								{#if i > 0}
+									{#if i === selectedDay}
+										<button
+											type="button"
+											on:click={() => {
+												selectDate(selectedYear, selectedMonth, i);
+											}}
+											class="flex justify-center items-center w-8 h-8 text-sm text-white bg-indigo-700 rounded sm:text-base hover:bg-indigo-500 focus:bg-indigo-500 focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2 focus:outline-none"
+											class:font-extrabold={inCurrentMonth && currentDay === i}>{i}</button
+										>
+									{:else}
+										<p class="text-sm font-medium text-gray-500 sm:text-base">
+											<button
+												class="border-none"
+												class:font-extrabold={inCurrentMonth && currentDay === i}
+												on:click={() => {
+													selectDate(selectedYear, selectedMonth, i);
+												}}
+											>
+												{i}
+											</button>
+										</p>
+									{/if}
+								{/if}
+							</div>
+						</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
 
 <style>
