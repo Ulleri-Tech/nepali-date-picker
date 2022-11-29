f1282b7afdb383a6bf2414e4fb2d132a1070e4cc
Binij Shrestha <shresthabinij@gmail.com>
2022-11-29 19:02:34 +0545

    show build file

---
+++ build/_app/immutable/chunks/1-4f9e2c58.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-f6fa9653.js";export{t as component};
---
+++ build/_app/immutable/chunks/2-db415d19.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-de777843.js";export{t as component};
